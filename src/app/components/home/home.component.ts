3665b20b04d0edc9e41d1f2e1319983c9800c7d8
theranjitkumar <web.ranjitkumar@gmail.com>
2018-12-30 01:08:40 +0530

    ngrx code added on home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Increment, Decrement, Reset } from '../../ngrx/counter.actions';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,7 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  count$: Observable<number>;
+
+  constructor(private store: Store<{ count: number }>) {
+      this.count$ = store.pipe(select('count'));
+  }
+
+  increment() {
+      this.store.dispatch(new Increment());
+  }
+
+  decrement() {
+      this.store.dispatch(new Decrement());
+  }
+
+  reset() {
+      this.store.dispatch(new Reset());
+  }
 
   ngOnInit() {
   }
--- src/app/testPages/ngrx-page/ngrx-page.component.ts
+++ src/app/testPages/ngrx-page/ngrx-page.component.ts
@@ -5,31 +5,38 @@ import { Observable } from 'rxjs';
 import { Increment, Decrement, Reset } from '../../ngrx/counter.actions';
 
 @Component({
-  selector: 'app-ngrx-page',
-  templateUrl: './ngrx-page.component.html',
-  styleUrls: ['./ngrx-page.component.css']
+    selector: 'app-ngrx-page',
+    templateUrl: './ngrx-page.component.html',
+    styleUrls: ['./ngrx-page.component.css']
 })
 export class NgrxPageComponent implements OnInit {
 
-  count$: Observable<number>;
+    count$: Observable<number>;
 
-  constructor(private store: Store<{ count: number }>) {
-    this.count$ = store.pipe(select('count'));
-  }
+    constructor(private store: Store<{ count: number }>) {
+        this.count$ = store.pipe(select('count'));
+    }
 
-  increment() {
-    this.store.dispatch(new Increment());
-  }
+    increment() {
+        this.store.dispatch(new Increment());
+    }
 
-  decrement() {
-    this.store.dispatch(new Decrement());
-  }
+    decrement() {
+        this.store.dispatch(new Decrement());
+    }
 
-  reset() {
-    this.store.dispatch(new Reset());
-  }
+    reset() {
+        this.store.dispatch(new Reset());
+    }
 
-  ngOnInit() {
-  }
+    sayHello() {
+        console.log('Hello dude.....');
+        const div = document.getElementById('div');
+        div.innerHTML = 'Hello dude....';
+    }
+
+    ngOnInit() {
+    this.sayHello();
+    }
 
 }
